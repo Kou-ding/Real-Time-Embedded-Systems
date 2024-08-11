d4a0c95b27148fdde7d1a438555c6daf9c7fb1d0
Kou <fotispapadak@gmail.com>
2024-08-11 18:59:47 +0300

    git ignore

---
+++ python_websockets/plot.bash
@@ -0,0 +1,8 @@
+# To create a virtual environment
+python3 -m venv my_venv
+# To activate the virtual environment
+source my_venv/bin/activate
+# Installing the necessary dependencies
+pip install websocket finnhub-python websocket-client pandas matplotlib
+# To run the code
+python3 csv_plotter.py
